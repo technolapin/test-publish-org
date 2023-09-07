8b33eac23117d6d4509710bd384e46bd9546a9b2
technolapin <clement.chomicki@protonmail.com>
2023-09-07 22:46:16 +0200

    now using a build folder; paths are still hard-coded

--- project.el
+++ project.el
@@ -2,7 +2,7 @@
 
 
 (setq org-latex-pdf-process
-      '("echo %O >> log.log && pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+      '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
         "biber %b"
         "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
         "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
@@ -11,33 +11,45 @@
 (setq org-publish-project-alist
       '(
 
-("org-notes"
- :base-directory "~/Documents/org-test/org/"
+("org-compile-html"
+ :base-directory "~/Documents/test-publish-org/org/"
  :base-extension "org"
- :publishing-directory "~/Documents/org-test/html/"
+ :publishing-directory "~/Documents/test-publish-org/html/"
  :recursive t
  :publishing-function org-html-publish-to-html
  :headline-levels 4
  :auto-preamble t
  )
-("org-latex"
- :base-directory "~/Documents/org-test/org/"
+
+("org-copy-to-build"
+ :base-directory "~/Documents/test-publish-org/org/"
+ :base-extension any
+ :publishing-directory "~/Documents/test-publish-org/build/"
+ :recursive t
+ :publishing-function org-publish-attachment
+ )
+
+("org-compile-latex"
+ :base-directory "~/Documents/test-publish-org/build/"
  :base-extension "org"
- :publishing-directory "~/Documents/org-test/pdf/"
+ :publishing-directory "~/Documents/test-publish-org/pdf/"
  :recursive t
  :publishing-function org-latex-publish-to-pdf
  :headline-levels 4
  :auto-preamble t
  )
 
-("org-static"
- :base-directory "~/Documents/org-test/org/"
+("org-copy-assets"
+ :base-directory "~/Documents/test-publish-org/org/"
  :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
- :publishing-directory "~/Documents/org-test/html/"
+ :publishing-directory "~/Documents/test-publish-org/html/"
  :recursive t
  :publishing-function org-publish-attachment
  )
 
-("org" :components ("org-latex" "org-notes" "org-static"))
+ ("org-latex" :components ("org-copy-to-build" "org-compile-latex"))
+ ("org-html"  :components ("org-compile-html" "org-copy-assets"))
+ 
+ ("org" :components ("org-latex" "org-html"))
 
 ))   
